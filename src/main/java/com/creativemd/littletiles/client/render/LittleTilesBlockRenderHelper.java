d44c81512827feb03806778b086c794896a409f8
Fabian Maurer <dark.shadow4@web.de>
2025-02-14 19:24:36 +0100

    Add functionality to fake blockId for shaders

--- src/main/java/com/creativemd/littletiles/client/render/LittleTilesBlockRenderHelper.java
+++ src/main/java/com/creativemd/littletiles/client/render/LittleTilesBlockRenderHelper.java
@@ -17,6 +17,7 @@ import com.creativemd.creativecore.client.rendering.ExtendedRenderBlocks;
 import com.creativemd.creativecore.client.rendering.RenderHelper3D;
 import com.creativemd.creativecore.common.utils.ColorUtils;
 import com.creativemd.creativecore.common.utils.CubeObject;
+import com.creativemd.littletiles.LittleTiles;
 
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
@@ -57,7 +58,13 @@ public class LittleTilesBlockRenderHelper {
                 fake.overrideMeta = cubes.get(i).meta;
                 extraRenderer.color = cubes.get(i).color;
                 extraRenderer.lockBlockBounds = true;
+                if (LittleTiles.angelicaCompat != null) {
+                    LittleTiles.angelicaCompat.setShaderMaterialOverride(cubes.get(i).block, cubes.get(i).meta);
+                }
                 extraRenderer.renderBlockAllFaces(cubes.get(i).block, x, y, z);
+                if (LittleTiles.angelicaCompat != null) {
+                    LittleTiles.angelicaCompat.resetShaderMaterialOverride();
+                }
                 extraRenderer.lockBlockBounds = false;
                 extraRenderer.color = ColorUtils.WHITE;
                 continue;

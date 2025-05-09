d2130039d56982d76d9c49d7bde85bc247a75a8a
Fabian Maurer <dark.shadow4@web.de>
2025-05-10 00:35:17 +0200

    Fix rounding

--- src/main/java/com/creativemd/littletiles/common/utils/LittleTileBlockPos.java
+++ src/main/java/com/creativemd/littletiles/common/utils/LittleTileBlockPos.java
@@ -45,10 +45,17 @@ public class LittleTileBlockPos {
         if (side == ForgeDirection.NORTH) {
             z -= 1.0 / 16;
         }
-        int subX = (int) ((x - (int) x) * 16);
-        int subY = (int) ((y - (int) y) * 16);
-        int subZ = (int) ((z - (int) z) * 16);
-        return new LittleTileBlockPos((int) x, (int) y, (int) z, subX, subY, subZ, side);
+        int subX = (int) Math.floor((x - Math.floor(x)) * 16);
+        int subY = (int) Math.floor((y - Math.floor(y)) * 16);
+        int subZ = (int) Math.floor((z - Math.floor(z)) * 16);
+        return new LittleTileBlockPos(
+                (int) Math.floor(x),
+                (int) Math.floor(y),
+                (int) Math.floor(z),
+                subX,
+                subY,
+                subZ,
+                side);
     }
 
     public int getPosX() {

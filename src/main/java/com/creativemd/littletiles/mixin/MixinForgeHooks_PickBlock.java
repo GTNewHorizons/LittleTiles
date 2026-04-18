db146ab78928f5802151c4aa0fb0185ff90916f5
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2026-04-18 20:08:50 +0200

    fix stupid memory leak in BlockTile

--- src/main/java/com/creativemd/littletiles/mixin/MixinForgeHooks_PickBlock.java
+++ src/main/java/com/creativemd/littletiles/mixin/MixinForgeHooks_PickBlock.java
@@ -1,9 +1,9 @@
 package com.creativemd.littletiles.mixin;
 
 import net.minecraft.block.Block;
-import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.world.World;
 import net.minecraftforge.common.ForgeHooks;
@@ -15,8 +15,8 @@ import org.spongepowered.asm.mixin.injection.At;
 import com.creativemd.creativecore.common.packet.PacketHandler;
 import com.creativemd.littletiles.LittleTiles;
 import com.creativemd.littletiles.common.BlockValidator;
-import com.creativemd.littletiles.common.blocks.BlockTile;
 import com.creativemd.littletiles.common.packet.LittleItemUpdatePacket;
+import com.creativemd.littletiles.common.tileentity.TileEntityLittleTiles;
 import com.creativemd.littletiles.common.utils.LittleTileBlock;
 import com.creativemd.littletiles.common.utils.LittleToolHandler;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
@@ -28,8 +28,6 @@ public class MixinForgeHooks_PickBlock {
     @Unique
     private static void littletiles$setBlock(ItemStack stack, Block block, int meta) {
         new LittleToolHandler(stack).setBlock(block, meta);
-        Minecraft mc = Minecraft.getMinecraft();
-
         PacketHandler.sendPacketToServer(new LittleItemUpdatePacket(stack.getTagCompound()));
     }
 
@@ -41,7 +39,7 @@ public class MixinForgeHooks_PickBlock {
             remap = false)
     private static ItemStack pickBlock(Block instance, MovingObjectPosition target, World world, int x, int y, int z,
             EntityPlayer player, Operation<ItemStack> original) {
-        ItemStack stack = player.getHeldItem();
+        final ItemStack stack = player.getHeldItem();
         if (stack != null && stack.getItem() == LittleTiles.chisel) {
             if (BlockValidator.isBlockValid(instance)) {
                 int meta = instance.getDamageValue(world, x, y, z);
@@ -49,14 +47,13 @@ public class MixinForgeHooks_PickBlock {
                 return null;
             }
             if (instance == LittleTiles.blockTile) {
-                if (BlockTile.loadTileEntity(world, x, y, z) && BlockTile.tempEntity.updateLoadedTile(player)) {
-                    LittleTileBlock tileBlock = (LittleTileBlock) BlockTile.tempEntity.loadedTile;
+                final TileEntity tileEntity = world.getTileEntity(x, y, z);
+                if (tileEntity instanceof TileEntityLittleTiles littleTile && littleTile.updateLoadedTile(player)) {
+                    final LittleTileBlock tileBlock = (LittleTileBlock) littleTile.loadedTile;
                     try {
                         littletiles$setBlock(stack, tileBlock.block, tileBlock.meta);
                         return null;
-                    } catch (Exception ignored) {
-
-                    }
+                    } catch (Exception ignored) {}
                 }
             }
         }

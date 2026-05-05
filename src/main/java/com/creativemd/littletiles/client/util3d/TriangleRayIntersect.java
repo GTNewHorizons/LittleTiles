e5ae97f65bcef1c5f2a93bb956f537df70c59781
Fabian Maurer <dark.shadow4@web.de>
2026-05-06 00:44:44 +0200

    spotless

--- src/main/java/com/creativemd/littletiles/client/util3d/TriangleRayIntersect.java
+++ src/main/java/com/creativemd/littletiles/client/util3d/TriangleRayIntersect.java
@@ -1,33 +1,17 @@
 /*
- * Copyright (c) 2009-2021 jMonkeyEngine
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are
- * met:
- *
- * * Redistributions of source code must retain the above copyright
- *   notice, this list of conditions and the following disclaimer.
- *
- * * Redistributions in binary form must reproduce the above copyright
- *   notice, this list of conditions and the following disclaimer in the
- *   documentation and/or other materials provided with the distribution.
- *
- * * Neither the name of 'jMonkeyEngine' nor the names of its contributors
- *   may be used to endorse or promote products derived from this software
- *   without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
- * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
- * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
- * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * Copyright (c) 2009-2021 jMonkeyEngine All rights reserved. Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following conditions are met: * Redistributions of source code
+ * must retain the above copyright notice, this list of conditions and the following disclaimer. * Redistributions in
+ * binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution. * Neither the name of 'jMonkeyEngine' nor the
+ * names of its contributors may be used to endorse or promote products derived from this software without specific
+ * prior written permission. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 package com.creativemd.littletiles.client.util3d;
@@ -35,7 +19,7 @@ package com.creativemd.littletiles.client.util3d;
 import org.joml.Vector3f;
 
 public final class TriangleRayIntersect {
-    
+
     private static float FLT_EPSILON = 0.0001f;
 
     public static float intersects(Vector3f origin, Vector3f direction, Vector3f v0, Vector3f v1, Vector3f v2) {
@@ -72,18 +56,16 @@ public final class TriangleRayIntersect {
         float diffEdge2Y = ((diffZ * edge2X) - (diffX * edge2Z));
         float diffEdge2Z = ((diffX * edge2Y) - (diffY * edge2X));
 
-        float dirDotDiffxEdge2 = sign * (direction.x * diffEdge2X
-            + direction.y * diffEdge2Y
-            + direction.z * diffEdge2Z);
+        float dirDotDiffxEdge2 = sign
+                * (direction.x * diffEdge2X + direction.y * diffEdge2Y + direction.z * diffEdge2Z);
 
         if (dirDotDiffxEdge2 >= 0.0f) {
             diffEdge2X = ((edge1Y * diffZ) - (edge1Z * diffY));
             diffEdge2Y = ((edge1Z * diffX) - (edge1X * diffZ));
             diffEdge2Z = ((edge1X * diffY) - (edge1Y * diffX));
 
-            float dirDotEdge1xDiff = sign * (direction.x * diffEdge2X
-                + direction.y * diffEdge2Y
-                + direction.z * diffEdge2Z);
+            float dirDotEdge1xDiff = sign
+                    * (direction.x * diffEdge2X + direction.y * diffEdge2Y + direction.z * diffEdge2Z);
 
             if (dirDotEdge1xDiff >= 0.0f) {
                 if (dirDotDiffxEdge2 + dirDotEdge1xDiff <= dirDotNorm) {

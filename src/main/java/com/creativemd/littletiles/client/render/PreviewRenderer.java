0761688d36a243699ec57596df87a12828c41035
Fabian Maurer <dark.shadow4@web.de>
2026-07-31 17:10:18 +0200

    Fixup

--- src/main/java/com/creativemd/littletiles/client/render/PreviewRenderer.java
+++ src/main/java/com/creativemd/littletiles/client/render/PreviewRenderer.java
@@ -241,7 +241,7 @@ public class PreviewRenderer {
                         Vec3 color = previewTile.getPreviewColor();
 
                         LittleToolHandler toolHandler;
-                        if (previewTile.preview != null) {
+                        if (previewTile.preview != null && mc.thePlayer.getHeldItem().getItem() != LittleTiles.chisel) {
                             toolHandler = new LittleToolHandler(previewTile.preview.nbt);
                         } else {
                             toolHandler = new LittleToolHandler(mc.thePlayer.getHeldItem());

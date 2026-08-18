1ee742a0e6e90ee62741d6b89555fda3aa0b3a0f
Fabian Maurer <dark.shadow4@web.de>
2026-08-18 21:51:41 +0200

    rename function

--- src/main/java/com/creativemd/littletiles/common/utils/LittleTile.java
+++ src/main/java/com/creativemd/littletiles/common/utils/LittleTile.java
@@ -490,7 +490,7 @@ public abstract class LittleTile {
             return null;
         }
         if (FMLCommonHandler.instance().getEffectiveSide().isClient()) {
-            return geometryCache.getSimpleMesh();
+            return geometryCache.getOrCreateSimpleMesh();
         }
         return Mesh3dUtil.meshFromTile(box, cutout);
     }

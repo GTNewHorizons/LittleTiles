14d8d44b8ca84d11c9d94c875a32ad04e024c195
Fabian Maurer <dark.shadow4@web.de>
2026-09-13 20:39:08 +0200

    Fix hammer

--- src/main/java/com/creativemd/littletiles/common/gui/SubContainerHammer.java
+++ src/main/java/com/creativemd/littletiles/common/gui/SubContainerHammer.java
@@ -33,6 +33,9 @@ public class SubContainerHammer extends SubContainer {
                     value.getByte("sizeX"),
                     value.getByte("sizeY"),
                     value.getByte("sizeZ"));
+            if (size.sizeX < 1 || size.sizeX > LittleTile.maxPos || size.sizeY < 1 || size.sizeY > LittleTile.maxPos
+                    || size.sizeZ < 1 || size.sizeZ > LittleTile.maxPos)
+                return;
             ItemStack stack = basic.getStackInSlot(0);
             if (stack != null && stack.getItem() instanceof ItemBlock) {
                 Block block = Block.getBlockFromItem(stack.getItem());
